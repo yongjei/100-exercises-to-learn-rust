0696f33a99e36aea1752b97b8fe697cfe16beb5b
Yongjei Kang <yongjei.kang@gmail.com>
2024-05-30 15:52:21 +0900

    2024.05.30. 통합 연습 문제 풀이 중 에러 발생.

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,64 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: f64,
+}
+
+impl Order {
+    pub fn new(new_product_name: &String, new_quantity: u32, new_unit_price: f64) -> Order {
+        let mut temp_order: Order = Order {
+            product_name: new_product_name.into(),
+            quantity: new_quantity,
+            unit_price: new_unit_price,
+        };
+
+        temp_order.product_name(new_product_name);
+        temp_order.quantity(new_quantity);
+        temp_order.unit_price(new_unit_price);
+        temp_order
+    }
+
+    pub fn product_name(&mut self, new_product_name: String) {
+        if new_product_name.is_empty() || new_product_name.len() > 300 {
+            panic!("The product_name can not be empty and it can not be longer than 300 characters.");
+        }
+
+        self.product_name = new_product_name;
+    }
+
+    pub fn get_product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&mut self, new_quantity: u32) {
+        if new_quantity <= 0 {
+            panic!("The quantity must be strictly greater than zero.");
+        }
+
+        self.quantity = new_quantity;
+    }
+
+    pub fn get_quantity(&self) -> u32 {
+        self.quantity
+    }
+
+    pub fn unit_price(&mut self, new_unit_price: f64) {
+        if new_unit_price <= 0.0 {
+            panic!("The unit_price is in cents and must be strictly gerater than zero.");
+        }
+
+        self.unit_price = new_unit_price;
+    }
+
+    pub fn get_unit_price(&self) -> f64 {
+        self.unit_price
+    }
+
+    pub fn total(&self) -> f64 {
+        self.unit_price * self.quantity as f64
+    }
+}
