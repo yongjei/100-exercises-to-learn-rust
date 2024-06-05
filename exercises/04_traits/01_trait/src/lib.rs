b97f3718978ca0bef9cad376037efbe763eaba73
Yongjei Kang <yongjei.kang@gmail.com>
2024-06-05 13:40:58 +0900

    2024.06.05

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -21,3 +21,19 @@ mod tests {
         assert!(!(-1).is_even());
     }
 }
+
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        self % 2 == 0
+    }
+}
